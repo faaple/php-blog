ba17573910a0d5b2d2bc22b750d038e21d594a94
Xinyu Wu <xinyuwu264@gmail.com>
2024-08-17 16:26:15 -0500

    language toggler

---
+++ langs/zh.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    // navigation
+    'login' => '登录',
+    'logout' => '退出',
+    'home' => '返回主页',
+    'all-posts' => '归档',
+    'new-post' => '新文章',
+    'greet' => '你好',
+    // index
+    'read-more' => '阅读全文',
+    'edit' => '编辑',
+    'comments' => '条评论',
+    'Comments' => '评论数量',
+    // edit post
+    'new-post' => '新建文章',
+    'edit-post' => '编辑文章',
+    'post-title' => '标题',
+    'post-body' => '正文',
+    'submit' => '提交',
+    'preview' => '预览',
+    'cancel' => '取消',
+    'rendered-markdown' => '渲染后的 Markdown',
+    // post list
+    'post-list' => '归档',
+    'creation-date' => '创建日期',
+    'delete' => '删除',
+    // comment
+    'name' => '用户名',
+    'website' => '网站',
+    'comment' => '输入评论',
+];
--- templates/top-menu.php
+++ templates/top-menu.php
@@ -1,16 +1,34 @@
+<?php 
+if (isset($_GET['lang'])) {
+	$_SESSION['lang'] = $_GET['lang'];
+} else if (!isset($_SESSION['lang'])) {
+	$_SESSION['lang'] = 'en';
+}
+$lang = $_SESSION['lang'];
+$get_word = include "langs/{$lang}.php";
+
+?>
 <div class="top-menu">
 	<div class="menu-options">
+		<span class="language-toggle">
+			<?php if ($lang === 'en'): ?>
+				<a href="?lang=zh">中文</a>
+			<?php elseif ($lang === 'zh'): ?>
+				<a href="?lang=en">English</a>
+			<?php endif ?>
+			||
+		</span>
 		<?php if (isLoggedIn()): ?>
-			<a href="index.php">Home</a>
+			<a href="index.php"><?php echo $get_word['home'] ?></a>
 			|
-			<a href="list-posts.php">All posts</a>
+			<a href="list-posts.php"><?php echo $get_word['all-posts'] ?></a>
 			|
-			<a href="edit-post.php">New post</a>
-			|
-			Hello <?php echo htmlspecialchars(getAuthUser()) ?>.
-			<a href="logout.php">Log out</a>
+			<a href="edit-post.php"><?php echo $get_word['new-post'] ?></a>
+			||
+			<?php echo $get_word['greet'] . ' ' . htmlspecialchars(getAuthUser()) ?>
+			<a href="logout.php"><?php echo $get_word['logout'] ?></a>
 		<?php else: ?>
-			<a href="login.php">Log in</a>
+			<a href="login.php"><?php echo $get_word['login'] ?></a>
 		<?php endif ?>
 	</div>
 </div>
